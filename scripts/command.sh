2f3032c83a46242b49eab4278decfbf55060aaa0
linhaojia13 <771235732@qq.com>
2023-06-29 16:44:20 +0800

    add git commands in daily work

--- scripts/command.sh
+++ scripts/command.sh
@@ -23,3 +23,40 @@ sh scripts/measure_latency_scanrefer.sh
 ## Nr3D 
 # train
 sh scripts/train_nr3d_2gpu.sh
+
+#########
+###git###
+#########
+## 每次git命令必须手敲，不能复制！
+## 最重要的事：1.时刻清楚自己是否处于工作分支; 2.已修改代码是否提交
+git branch --list                # 查看当前仓库有哪些分支，以及正处在哪个分支
+git checkout RES/spformer        # 切换到工作分支
+git status                       # 查看当前代码修改状态、提交状态
+
+## 提交修改
+git add . 
+git commit -m "xxxxx"
+
+## 创建工作分支。创建新分支前要确保已经提交了修改。
+git checkout main                # 切换到主分支
+git pull origin main             # 拉取主分支的最新更新
+git branch RES/spformer          # 创建工作分支
+git checkout RES/spformer        # 切换到工作分支
+git branch RES/spformer/spunet-backbone # 创建工作分支
+
+## 如果远程仓库上还没有这个工作分支（例如你刚刚在本地创建了这个分支），你可能需要先将本地分支推送到远程仓库并设置追踪关系：
+git push -u origin RES/spformer
+git push -u origin RES/spformer/spunet-backbone
+
+## 日常与远程仓库保持同步
+git checkout main                # 切换到主分支
+git pull origin main             # 拉取主分支的最新更新
+
+git checkout RES/spformer        # 切换到工作分支
+git pull origin RES/spformer     # 拉取工作分支的最新更新
+
+git add .
+git commit -m "xxxxx"            # 合并前确保当前修改已经提交
+
+git merge main                   # 合并主分支的更新到工作分支
+git push origin RES/spformer     # 推送更新到远程工作分支
