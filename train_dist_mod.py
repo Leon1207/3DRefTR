d4872c6fc1395200d182761316db75baac152121
linhaojia13 <771235732@qq.com>
2023-07-07 12:19:18 +0800

    cp joint_det_dataset.py as joint_det_dataset_superpoint.py

--- train_dist_mod.py
+++ train_dist_mod.py
@@ -18,7 +18,8 @@ import torch.distributed as dist
 
 from main_utils import parse_option, BaseTrainTester
 from data.model_util_scannet import ScannetDatasetConfig
-from src.joint_det_dataset import Joint3DDataset
+# from src.joint_det_dataset import Joint3DDataset
+from src.joint_det_dataset_superpoint import Joint3DDataset
 from src.grounding_evaluator import GroundingEvaluator
 from models import BeaUTyDETR, BeaUTyDETR_spunet
 from models import APCalculator, parse_predictions, parse_groundtruths
